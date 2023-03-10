2abf4919f15fc6e4733e7a4f33e58f75feae744c
djumanov <diyorbekjumanov1120@gmail.com>
2023-03-10 17:33:01 +0500

    add all smartphone

--- main.py
+++ main.py
@@ -2,34 +2,25 @@
 import requests
 from tinydb import TinyDB
 from pprint import pprint
-# Define a function to get the data from the database
-def get_data(path: str ,brand: str):
-    # Get the data from the database
-    db = TinyDB('db.json')
-  
-    # Get list of tables
-    tables = db.tables()
-    data = {}
-    for table in tables:
-        data[table] = db.table(table).all()
-    
-    # # Return the data
-    return data
 
-db = get_data(path='db.json',brand='apple')
 
-# Add the data to the database through the API
+db = TinyDB('db.json')
 
-BASE_URL = 'http://localhost:8000/api/add'
-for data in db.values():
-    for idx,item in enumerate(data):
-        item['name']=item['brend'] 
-        price = item['price']        
-        item['price']=float(price[:-5].replace(' ',''))
-        response = requests.post(BASE_URL, json=item)
+tables = db.tables()
         
-        # Print progress to the console
-        print(f'Progress: {idx+1}/{len(data)} \t Status: {response.status_code}')
+c = 0
+for table in tables:
+    collection = db.table(table)
+    smartphones = collection.all()
+    for smartphone in smartphones:
+        smartphone['price'] = float(smartphone['price'][:-5].replace(' ', ''))
+        smartphone['name'] = smartphone['brend']
+        smartphone['ram'] = int(smartphone['ram'])
+        smartphone['memory'] = int(smartphone['memory'])
+        smartphone.pop('brend')
+        c += 1
         
-        
+        response = requests.post('http://127.0.0.1:8000/add/', json=smartphone)
+        print(smartphone['name'], response.status_code)
+print(c)
 
